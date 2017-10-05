95574bcce7b107751dc110a4595a4dc5299e4042
Nader Samadyan <Nadd@dhcp-128-189-211-110.ubcsecure.wireless.ubc.ca>
2017-10-04 19:19:26 -0700

    now addDataset works, however on the implemented tests there is a weird behavior, the data correctly gets written on disk as a JSON file but there is an error with regards to the format of the file.

--- src/controller/InsightFacade.ts
+++ src/controller/InsightFacade.ts
@@ -4,7 +4,9 @@
 import {IInsightFacade, InsightResponse} from "./IInsightFacade";
 
 import Log from "../Util";
+
 var JSZip = require("jszip");
+var fs = require('fs');
 
 export default class InsightFacade implements IInsightFacade {
 
@@ -13,26 +15,77 @@ export default class InsightFacade implements IInsightFacade {
     }
 
 
+
     addDataset(id: string, content: string): Promise<InsightResponse> {
+        var resp: InsightResponse = {
+            code: 0,
+            body: {}
+        }
+        if (fs.existsSync("Data_Set/MyDatasetInsight"+id+".json")) {
+            resp.code = 201;
+            fs.unlink("Data_Set/MyDatasetInsight"+id+".json");
+        }else{
+            resp.code = 204;
+        }
+
+        var stream = fs.createWriteStream("Data_Set/MyDatasetInsight"+id+".json");
+        stream.on('error', console.error);
+
         return new Promise(function (fulfill, reject) {
             JSZip.loadAsync(content, {base64: true}).then(function (zip: any) {
-                Object.keys(zip.files).forEach(function (relativePath:any, zipEntry:any) {  // 2) print entries
-                    zip.files[relativePath].async("string")
-                .then(function (txt: string) {
-                    console.log(txt);
-                    fulfill();
-                }).catch(function (err: string) {
-                    reject(err);
+                Object.keys(zip.files).forEach(function (relativePath:any, zipEntry:any) {
+                    zip.files[relativePath].async("string").then(function (txt: string) {
+
+                        let obj = JSON.parse(txt);      // turning the string into a JSON object
+                        obj = obj['result'];            // to exclude the section labled 'rank' which we dont need
+
+                        for(let i of obj){
+                            // requirement is only 9 fields of the 36 so we can selectively thake the 9 by
+                            // the following mapping
+                            var dict = {
+                                "courses_dept" : i['Subject'],
+                                "courses_id" : i['Course'],
+                                "courses_avg" : i['Avg'],
+                                "courses_instructor" : i['Professor'],
+                                "courses_title" : i['Title'],
+                                "courses_pass" : i['Pass'],
+                                "courses_fail" : i['Fail'],
+                                "courses_audit" : i['Audit'],
+                                "courses_uuid" : i['id'].toString()
+                            };
+
+                            var dictstring = JSON.stringify(dict); // to be able to write it back on disk we will make it
+                                                                    // a string.
+                            try{
+                                stream.write(dictstring + ';' +'\n');
+                                //fulfill(resp);
+                            }catch (err){
+                                console.log('1');
+                                resp.code = 400;
+                                resp.body = {err};
+                                reject(resp);
+                            }
+
+                            //console.log(dict);
+                        }
+
+                        //console.log(obj['result']);
+                    }).catch(function (err: string) {
+                        console.log('2');
+                        console.log(err);
+                        resp.code = 400;
+                        resp.body = {err};
+                        reject(resp);
+                    })
                 })
-            })
+                fulfill(resp);
             }).catch(function (err: string) {
-                console.log(err);
-                reject(err);
+                console.log('3');
+                resp.code = 400;
+                resp.body = {err};
+                reject(resp);
             })
-
-
         });
-
     }
 
     removeDataset(id: string): Promise<InsightResponse> {
--- test/InsightSpec.ts
+++ test/InsightSpec.ts
@@ -2,6 +2,7 @@
 import InsightFacade from "../src/controller/InsightFacade"
 import {InsightResponse} from "../src/controller/IInsightFacade";
 import {expect} from 'chai';
+import Log from "../src/Util";
 
 
 
@@ -17,12 +18,26 @@ describe("InsightSpec", function () {
         insightFacade = null;
     });
 
-    it ("Just Test to access console log stuff in addDataset" , function () {
+    it ("adding the dataset for the first time to make sure that it creates the json file with code 204" , function () {
         this.timeout(10000);
         let fs   = require('fs');
         let data = fs.readFileSync('test/courses.zip');
         return insightFacade.addDataset("20", data.toString('base64')).then(function(insightResponse: InsightResponse){
+            Log.test('Code: ' + insightResponse.code);
+            expect(insightResponse.code).to.deep.equal(204);
+        }).catch(function (insightResponse: InsightResponse) {
+            console.log("in catch... the promise returned by addDataSet rejected by the error code: " + insightResponse.code);
+        })
 
+    });
+
+    it ("adding the dataset for the second time to make sure that it replaces the json file with code 201" , function () {
+        this.timeout(10000);
+        let fs   = require('fs');
+        let data = fs.readFileSync('test/courses.zip');
+        return insightFacade.addDataset("20", data.toString('base64')).then(function(insightResponse: InsightResponse){
+            Log.test('Code: ' + insightResponse.code);
+            expect(insightResponse.code).to.deep.equal(201);
         }).catch(function (insightResponse: InsightResponse) {
             console.log("in catch... the promise returned by addDataSet rejected by the error code: " + insightResponse.code);
         })
