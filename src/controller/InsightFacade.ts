7281870520f5b3f5bcadeebe00b7f734888deaa5
Nader Samadyan <Nadd@dhcp-128-189-211-214.ubcsecure.wireless.ubc.ca>
2017-10-05 12:44:40 -0700

    addDataset is working with no errors know, we have to put the added json objects to an array yet

--- src/controller/InsightFacade.ts
+++ src/controller/InsightFacade.ts
@@ -17,16 +17,33 @@ export default class InsightFacade implements IInsightFacade {
 
 
     addDataset(id: string, content: string): Promise<InsightResponse> {
+        function isJsonString(str: string) {
+            try {
+                JSON.parse(str);
+            } catch (e) {
+                return false;
+            }
+            return true;
+        }
+
         var resp: InsightResponse = {                                                       // Init a new Insight Response
             code: 0,                                                                        // With an initial code of 0
             body: {}                                                                        // and an empty body
         }
+
+        if (!fs.existsSync("./Data_Set")) {
+            // create the directory for the dataset if it does not exits already
+            fs.mkdirSync("./Data_Set")                       // create directory
+        }
+
         if (fs.existsSync("Data_Set/MyDatasetInsight"+id+".json")) {                 // If file exists,
             resp.code = 201;                                                                // operation will be successful; id exists
-            fs.unlink("Data_Set/MyDatasetInsight"+id+".json");                       // Remove file.
+            //fs.unlinkSync("./Data_Set/MyDatasetInsight"+id+".json");                       // Remove file.
         }else{                                                                              // If file doesn't exist,
             resp.code = 204;                                                                // operation will be successful; new id
         }
+
+
 
         var stream = fs.createWriteStream("Data_Set/MyDatasetInsight"+id+".json");   // Init a Writable Stream object.
         stream.on('error', console.error);                                          // On error, log the error.
@@ -35,40 +52,49 @@ export default class InsightFacade implements IInsightFacade {
             JSZip.loadAsync(content, {base64: true}).then(function (zip: any) {                         // Read ZIP, check validity. If valid,
                 Object.keys(zip.files).forEach(function (relativePath:any, zipEntry:any) {  // Check each key in the Object read from ZIP
                     zip.files[relativePath].async("string").then(function (txt: string) {              // Get String with Promise.
+                        if(isJsonString(txt)) {
+                            var obj = JSON.parse(txt);
+                            obj = obj['result'];
 
-                        let obj = JSON.parse(txt);      // turning the string into a JSON object
-                        obj = obj['result'];            // to exclude the section labled 'rank' which we dont need
+                            // turning the string into a JSON object
+                            // to exclude the section labled 'rank' which we dont need
 
-                        for(let i of obj){
-                            // requirement is only 9 fields of the 36 so we can selectively thake the 9 by
-                            // the following mapping
-                            var dict = {
-                                "courses_dept" : i['Subject'],
-                                "courses_id" : i['Course'],
-                                "courses_avg" : i['Avg'],
-                                "courses_instructor" : i['Professor'],
-                                "courses_title" : i['Title'],
-                                "courses_pass" : i['Pass'],
-                                "courses_fail" : i['Fail'],
-                                "courses_audit" : i['Audit'],
-                                "courses_uuid" : i['id'].toString()
-                            };
+                            for (let i of obj) {
+                                // requirement is only 9 fields of the 36 so we can selectively thake the 9 by
+                                // the following mapping
+                                var dict = {
+                                    "courses_dept": i['Subject'],
+                                    "courses_id": i['Course'],
+                                    "courses_avg": i['Avg'],
+                                    "courses_instructor": i['Professor'],
+                                    "courses_title": i['Title'],
+                                    "courses_pass": i['Pass'],
+                                    "courses_fail": i['Fail'],
+                                    "courses_audit": i['Audit'],
+                                    "courses_uuid": i['id'].toString()
+                                };
 
-                            var dictstring = JSON.stringify(dict);  // to be able to write it back on disk we will make it
-                                                                    // a string.
-                            try{                                                // Try to..
-                                stream.write(dictstring + ';' +'\n');   // Write on disk.
-                                //fulfill(resp);
-                            }catch (err){                                       // In case of error
-                                console.log('1');                               // Log it.
-                                resp.code = 400;                                // Update with error code.
-                                resp.body = {err};                              // Update with error body.
-                                reject(resp);                                   // Return a rejected Promise.
+                                var dictstring = JSON.stringify(dict);// to be able to write it back on disk we will make it
+                                // a string.
+                                //console.log(dictstring);
+
+
+                                try {                                                // Try to..
+                                    // Write on disk.
+                                    stream.write(dictstring + '\n');
+                                    //fulfill(resp);
+                                } catch (err) {                                       // In case of error
+                                    console.log('1');                               // Log it.
+                                    resp.code = 400;                                // Update with error code.
+                                    resp.body = {err};                              // Update with error body.
+                                    reject(resp);                                   // Return a rejected Promise.
+                                }
+
+
+                                //console.log(dict);
                             }
-
-                            //console.log(dict);
                         }
-
+                        fulfill();
                         //console.log(obj['result']);
                     }).catch(function (err: string) {                           // If String can't be obtained,
                         console.log('2');                                       // Log it.
@@ -80,7 +106,8 @@ export default class InsightFacade implements IInsightFacade {
                 });
                 fulfill(resp);                                                  // Finally, fulfill.
             }).catch(function (err: string) {                                   // If ZIP is not valid
-                console.log('3');                                               // Log it.
+                console.log('3');
+                console.log(err);// Log it.
                 resp.code = 400;                                                // Update with error code.
                 resp.body = {err};                                              // Update with error body.
                 reject(resp);                                                   // Reject.
