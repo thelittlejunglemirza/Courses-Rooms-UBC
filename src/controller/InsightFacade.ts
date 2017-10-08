d7980920f2d3b6ffbc807cf76ee7d1b839815e3a
Nader Samadyan <Nadd@dhcp-128-189-212-62.ubcsecure.wireless.ubc.ca>
2017-10-07 18:53:19 -0700

    the implementation for addDataset and removeDataset are done, the local tests pass succesfully for valid and invalid datasets, overwrite operations are succesful as well, lets hope for the best, god bless

--- src/controller/InsightFacade.ts
+++ src/controller/InsightFacade.ts
@@ -33,53 +33,39 @@ export default class InsightFacade implements IInsightFacade {
             code: 0,                                                                        // With an initial code of 0
             body: {}                                                                        // and an empty body
         };
-
         if (!fs.existsSync("./Data_Set")) {                                          // Create directory if not found.
             fs.mkdirSync("./Data_Set")
         }
 
-        if (fs.existsSync("Data_Set/MyDatasetInsight"+id+".json")) {                 // If file exists,
-            resp.code = 201;                                                                // operation will be successful; id exists
-        }else{                                                                              // If file doesn't exist,
-            resp.code = 204;                                                                // operation will be successful; new id
-        }
-
-
-
-        let stream = fs.createWriteStream("Data_Set/MyDatasetInsight"+id+".json");               // Init a Writable Stream object.
-        stream.on('error', console.error);                                                      // On error, log the error.
-
         return new Promise(function (fulfill, reject) {                                                 // Return Promise:
             JSZip.loadAsync(content, {base64: true}).then(function (zip: any) {                         // Read ZIP, check validity. If valid,
-                let pArr:Array<Promise<any>> = [];                                                      // Init Promise Array
-                /*
-                if(zip.files === null) {                                                                // TODO: Check empty ZIP(?)
-                    console.log('0');                                                                   // Log-tracking(0).
-                    resp.code = 400;                                                                    // Update with error code.
-                    reject(resp);                                                                       // Reject the Promise.
+                if (fs.existsSync("Data_Set/MyDatasetInsight"+id+".json")) {                 // If file exists,
+                    resp.code = 201;                                                                // operation will be successful; id exists
+                    fs.unlinkSync("./Data_Set/MyDatasetInsight"+id+".json");                       // Remove file.
+                }else{                                                                              // If file doesn't exist,
+                    resp.code = 204;                                                                // operation will be successful; new id
                 }
-                */
+                var stream = fs.createWriteStream("Data_Set/MyDatasetInsight"+id+".json");   // Init a Writable Stream object.
+                stream.on('error', console.error);                                             // On error, log the error.
+
+                var pArr:Array<Promise<any>> = [];                                                      // Init Promise Array
                 Object.keys(zip.files).forEach(function (relativePath:any, zipEntry:any) {  // Check each key in the Object read from ZIP
                     pArr.push(zip.files[relativePath].async("string"));                                 // Attempt to get String Promises
                 });
                 Promise.all(pArr).then(function (txtArr) {                                  // Check all String Promises for fulfillment.
-                    let flagEndOfTexts = false;                                                         // Init flags.
-                    let flagEnd = false;
+                    let flagFoundCourse = false;
                     stream.write('[' + '\n');                                                   // Write the beginning of the array (open).
                     let sep = "";                                                                       // Init separator.
                     for (let i of txtArr) {                                                             // For each String in Promise Array
-                        if(i == txtArr[txtArr.length-1]){                                               // If the end has been reached,
-                            flagEndOfTexts = true;                                                      // Set flag to true.
-                        }
                         if (isJsonString(i)) {                                                          // If parseable,
                             let obj = JSON.parse(i);                                                    // Parse it.
                             obj = obj['result'];
                             // turning the string into a JSON object
                             // to exclude the section labled 'rank' which we dont need
+                            if(flagFoundCourse == false && obj.length > 0){
+                                flagFoundCourse = true;
+                            }
                             for (let j of obj) {                                                        // Cycle through JSON
-                                if(j == obj[obj.length-1] && flagEndOfTexts == true){                   // Check for end of JSON
-                                    flagEnd = true;                                                     // Flag end of JSON
-                                }
                                 // requirement is only 9 fields of the 36 so we can selectively thake the 9 by
                                 // the following mapping
                                 let dict = {
@@ -101,16 +87,14 @@ export default class InsightFacade implements IInsightFacade {
                                     sep = ',\n'
                                 }
                             }
-                            stream.write('\n]');                                // Close off the array.
-                            fulfill(resp);                                              // Finally, fulfill.
                         }
-                        /*                                                              // TODO: CASE: NOT PARSEABLE ELSE
-                        else{
-                            resp.code = 400;
-                            reject(resp);
-                        }
-                        */
                     }
+                    stream.write('\n]');                                // Close off the array.
+                    if(flagFoundCourse == false){
+                        fs.unlinkSync("./Data_Set/MyDatasetInsight"+id+".json");
+                        throw "the dataset is not valid";
+                    }
+                    fulfill(resp);                                              // Finally, fulfill.
                 }).catch(function (err) {
                     console.log('1');                                           // Log-tracking(1).
                     console.log(err);                                           // Log error.
@@ -129,7 +113,25 @@ export default class InsightFacade implements IInsightFacade {
     }
 
     removeDataset(id: string): Promise<InsightResponse> {
-        return null;
+        var resp: InsightResponse = {                                                       // Init a new Insight Response
+            code: 0,                                                                        // With an initial code of 0
+            body: {}                                                                        // and an empty body
+        };
+        return new Promise(function (fulfill, reject) {
+            if (!fs.existsSync("./Data_Set")) {
+                resp.code = 404;                     // there is not sets
+                reject(resp);
+            }else {
+                if(!fs.existsSync("Data_Set/MyDatasetInsight"+id+".json")){
+                    resp.code = 404;                     // there is no dataset with this id
+                    reject(resp);
+                }else {
+                    fs.unlink("./Data_Set/MyDatasetInsight"+id+".json");
+                    resp.code = 204;
+                    fulfill(resp);
+                }
+            }
+        });
     }
 
     performQuery(query: any): Promise <InsightResponse> {
--- test/InsightSpec.ts
+++ test/InsightSpec.ts
@@ -22,27 +22,42 @@ describe("InsightSpec", function () {
         insightFacade = null;
     });
 
+    it ("removing a dataset when directory does not exist with code 404 will fail" , function () {
+        this.timeout(10000);
+        return insightFacade.removeDataset("11").then(function(insightResponse: InsightResponse){
+            expect.fail();
+        }).catch(function (insightResponse: InsightResponse) {
+            console.log("in catch:");
+            console.log("the promise returned by addDataSet rejected by the error code: " + insightResponse.code);
+            console.log("and error body: " + insightResponse.body);
+            expect(insightResponse.code).to.deep.equal(404);
+        })
+
+    });
+
     it ("adding the dataset for the first time to make sure that it creates the json file with code 204" , function () {
         this.timeout(10000);
-        let data = fs.readFileSync('test/courses.zip');
-        return insightFacade.addDataset("20", data.toString('base64')).then(function(insightResponse: InsightResponse){
+        let data = fs.readFileSync('test/1.zip');
+        return insightFacade.addDataset("1", data.toString('base64')).then(function(insightResponse: InsightResponse){
             Log.test('Code: ' + insightResponse.code);
             expect(insightResponse.code).to.deep.equal(204);
         }).catch(function (insightResponse: InsightResponse) {
-            console.log("in catch... the promise returned by addDataSet rejected by the error code: " + insightResponse.code);
+            console.log("in catch:");
+            console.log("the promise returned by addDataSet rejected by the error code: " + insightResponse.code);
             expect.fail();
         })
 
     });
 
-    it ("adding the dataset for the second time to make sure that it replaces the json file with code 201" , function () {
+    it ("adding a dataset that is much shorter, expecting code 204" , function () {
         this.timeout(10000);
-        let data = fs.readFileSync('test/courses.zip');
-        return insightFacade.addDataset("20", data.toString('base64')).then(function(insightResponse: InsightResponse){
+        let data = fs.readFileSync('test/2.zip');
+        return insightFacade.addDataset("2", data.toString('base64')).then(function(insightResponse: InsightResponse){
             Log.test('Code: ' + insightResponse.code);
-            expect(insightResponse.code).to.deep.equal(201);
+            expect(insightResponse.code).to.deep.equal(204);
         }).catch(function (insightResponse: InsightResponse) {
-            console.log("in catch... the promise returned by addDataSet rejected by the error code: " + insightResponse.code);
+            console.log("in catch:");
+            console.log("the promise returned by addDataSet rejected by the error code: " + insightResponse.code);
             expect.fail();
         })
 
@@ -51,7 +66,7 @@ describe("InsightSpec", function () {
     it ("Adding a valid ZIP with no data at all should return an error code of 400." , function () {
         this.timeout(10000);
         let data = fs.readFileSync('test/nocourses.zip');
-        return insightFacade.addDataset("20", data.toString('base64')).then(function(insightResponse: InsightResponse){
+        return insightFacade.addDataset("10", data.toString('base64')).then(function(insightResponse: InsightResponse){
             Log.test('Code: ' + insightResponse.code);
             expect.fail();
         }).catch(function (insightResponse: InsightResponse) {
@@ -61,10 +76,66 @@ describe("InsightSpec", function () {
 
     });
 
+    it ("adding a valid zip dataset that has no courses" , function () {
+        this.timeout(10000);
+        let data = fs.readFileSync('test/in1.zip');
+        return insightFacade.addDataset("3", data.toString('base64')).then(function(insightResponse: InsightResponse){
+            expect.fail();
+        }).catch(function (insightResponse: InsightResponse) {
+            console.log("in catch:");
+            console.log("the promise returned by addDataSet rejected by the error code: " + insightResponse.code);
+            console.log("and error body: " + insightResponse.body);
+            expect(insightResponse.code).to.deep.equal(400);
+        })
+
+    });
+
+    it ("this is a dataset added for the next test!" , function () {
+        this.timeout(10000);
+        let fs   = require('fs');
+        let data = fs.readFileSync('test/1.zip');
+        return insightFacade.addDataset("4", data.toString('base64')).then(function(insightResponse: InsightResponse){
+            expect(insightResponse.code).to.deep.equal(204);
+        }).catch(function (insightResponse: InsightResponse) {
+            expect.fail();
+        })
+
+    });
+
+    it ("adding a second dataset that is much shorter, expecting code 201, and correct overwrite of existing dataset" , function () {
+        this.timeout(10000);
+        let fs   = require('fs');
+        let data = fs.readFileSync('test/2.zip');
+        return insightFacade.addDataset("4", data.toString('base64')).then(function(insightResponse: InsightResponse){
+            Log.test('Code: ' + insightResponse.code);
+            expect(insightResponse.code).to.deep.equal(201);
+        }).catch(function (insightResponse: InsightResponse) {
+            console.log("in catch:");
+            console.log("the promise returned by addDataSet rejected by the error code: " + insightResponse.code);
+            expect.fail();
+        })
+
+    });
+
+
+    it ("adding an invalid zip file should fail with error code 400" , function () {
+        this.timeout(10000);
+        let data = fs.readFileSync('test/notzip.rtf');
+        return insightFacade.addDataset("5", data.toString('base64')).then(function(insightResponse: InsightResponse){
+            expect.fail();
+        }).catch(function (insightResponse: InsightResponse) {
+            console.log("in catch:");
+            console.log("the promise returned by addDataSet rejected by the error code: " + insightResponse.code);
+            console.log("and error body: " + insightResponse.body);
+            expect(insightResponse.code).to.deep.equal(400);
+        })
+
+    });
+
     it ("Adding an invalid ZIP should return error code 400." , function () {
         this.timeout(10000);
         let data = fs.readFileSync('test/notazip');
-        return insightFacade.addDataset("20", data.toString('base64')).then(function(insightResponse: InsightResponse){
+        return insightFacade.addDataset("6", data.toString('base64')).then(function(insightResponse: InsightResponse){
             Log.test('Code: ' + insightResponse.code);
             expect.fail();
         }).catch(function (insightResponse: InsightResponse) {
@@ -77,7 +148,7 @@ describe("InsightSpec", function () {
     it ("Adding a valid ZIP with invalid data should return error code 400." , function () {
         this.timeout(10000);
         let data = fs.readFileSync('test/invalid.zip');                             // 1 file, {] <- invalid JSON
-        return insightFacade.addDataset("20", data.toString('base64')).then(function(insightResponse: InsightResponse){
+        return insightFacade.addDataset("7", data.toString('base64')).then(function(insightResponse: InsightResponse){
             Log.test('Code: ' + insightResponse.code);
             expect.fail();
         }).catch(function (insightResponse: InsightResponse) {
@@ -86,5 +157,32 @@ describe("InsightSpec", function () {
         })
 
     });
+
+    it ("removing a dataset that is previously added" , function () {
+        this.timeout(10000);
+        return insightFacade.removeDataset("4").then(function(insightResponse: InsightResponse){
+            Log.test('Code: ' + insightResponse.code);
+            expect(insightResponse.code).to.deep.equal(204);
+        }).catch(function (insightResponse: InsightResponse) {
+            console.log("in catch:");
+            console.log("the promise returned by addDataSet rejected by the error code: " + insightResponse.code);
+            expect.fail();
+        })
+
+    });
+
+    it ("removing a dataset that does not exist, error code 404" , function () {
+        this.timeout(10000);
+        return insightFacade.removeDataset("4").then(function(insightResponse: InsightResponse){
+            expect.fail();
+        }).catch(function (insightResponse: InsightResponse) {
+            console.log("in catch:");
+            console.log("the promise returned by addDataSet rejected by the error code: " + insightResponse.code);
+            console.log("and error body: " + insightResponse.body);
+            expect(insightResponse.code).to.deep.equal(404);
+        })
+
+    });
+
 
 });
